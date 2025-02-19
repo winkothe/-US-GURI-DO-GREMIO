204f198f65bfdc4be2b18acbdc0e2bdc71b97db9
winkothe <145023356+winkothe@users.noreply.github.com>
2025-02-19 12:31:45 -0300

    Add files via upload
    19/02/2025

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const aumentaFonteBotao = document.getElementByld('aumentar-fonte');
+
+
+    let tamanhoAtualFonte = 1;
+    aumentaFonteBotao.addEventListener('click', function(){
+        tamanhoAtualFonte
+    })
+})frasncisco lives
