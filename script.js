dfba84d1a750649ab1cc17303edd7f75fa1ac4e3
winkothe <145023356+winkothe@users.noreply.github.com>
2025-04-09 11:04:20 -0300

    Add files via upload
    09/04/2025

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function(){
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+    const alternaContraste = document.getElementById('alterna-contraste');
 
 
     let tamanhoAtualFonte = 1;
@@ -12,8 +13,22 @@ document.addEventListener('DOMContentLoaded', function(){
     diminuiFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte -= 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
-    });
+    })
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
+    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade');
+    botaoDeAcessibilidade.addEventListener('click', function (){
+    botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+    opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
+    
 });
-const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
-const opcoesDeAcessibilidade = document.getElementById('opcoes-acssibilidade');
-botaoDeAcessilibidade.addEventListenerListener('click')
+
+alternaContraste.addEventListener('click', function(){
+    document.body.classList.toggle('alto-contraste')
+})
+
+});
+
+scrollReveal().reveal('#inicio', {delay: 500})
+scrollReveal().reveal('#torpicalia', {delay: 500})
+scrollReveal().reveal('#galeria', {delay: 500})
+scrollReveal().reveal('#contato', {delay: 500})
